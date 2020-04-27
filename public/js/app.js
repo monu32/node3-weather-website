c0b3c451c4f46f11322e49ce4850b353608d8afd
monu_vishwakarma <monu786vish@gmail.com>
2020-04-28 00:51:54 +0530

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!')
+//console.log('Client side javascript file is loaded!')
 
 fetch('http://localhost:3000/weather?address=bhopal').then((response) => {
     response.json().then((data) => {
